b4d39a2d666b6bcd69422b0520cc93dc9bb0ebde
Unknown <a.canutperdigon@gmail.com>
2018-02-12 20:18:13 +0100

    Efecto de espejo.

---
+++ Assets/Espejo_Proyecto/Mirror_Reflection.shader
@@ -0,0 +1,49 @@
+﻿Shader "Custom/Mirror_Reflection" {
+	Properties{
+		_MainTexture("Mirror reflection texture", 2D) = "white" {}
+	}
+
+	SubShader{
+		Tags{
+			"Queue" = "Transparent"
+			"PreviewType" = "Plane"
+		}
+
+		Pass{
+			ZWrite Off
+			Cull Off
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			CGPROGRAM
+			#pragma vertex vertexShader;
+			#pragma fragment fragmentShader;
+			#include "UnityCG.cginc"
+
+			struct input {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				half4 color : COLOR;
+			};
+
+			struct vertexOutuput {
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				half4 color : COLOR;
+			};
+
+			vertexOutuput vertexShader(input i) {
+				vertexOutuput o;
+				o.vertex = UnityObjectToClipPos(i.vertex);
+				o.uv = i.uv;
+				o.color = i.color;
+				return o;
+			}
+
+			sampler2D _MainTexture;
+			float4 fragmentShader(vertexOutuput i) {
+				return tex2D(_MainTexture, i.uv) * i.color;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Espejo_Proyecto/Reflection_Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Reflection_Camera : MonoBehaviour {
+    [HideInInspector]
+    [SerializeField]
+    private Camera _camera;
+    private int _downResFactor = 1;
+
+    private string _globalReflexionName = "_globalReflexionTexture";
+	
+	// Update is called once per frame
+	void Update () {
+        GenerateRT();
+	}
+
+    void GenerateRT(){
+        _camera = GetComponent<Camera>();
+
+        if(_camera.targetTexture != null){
+            RenderTexture tmp = _camera.targetTexture;
+            _camera.targetTexture = null;
+            DestroyImmediate(tmp);
+        }
+
+        _camera.targetTexture = new RenderTexture(_camera.pixelWidth >> _downResFactor, _camera.pixelHeight >> _downResFactor, 16);
+        _camera.targetTexture.filterMode = FilterMode.Bilinear;
+
+        Shader.SetGlobalTexture(_globalReflexionName, _camera.targetTexture);
+    }
+}
